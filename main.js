8234522095e5f673f03db918be99d415ecd53c32
SHRAVAN KUMAR <shaliniprasad489@gmail.com>
2021-08-24 21:43:11 +0530

    added themes

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ const equaltoBtn = document.querySelector(".equalto");
 equaltoBtn.addEventListener("click", () => {
   screenTxt.value = eval(screenTxt.value)
 })
+
+// active1
+let two =document.getElementById("two");
+two.addEventListener("click", function(){
+  // console.log("hi");
+  let body = document.querySelector('body');
+  body.classList.add('active1');
+  body.classList.remove('active2');
+})
+
+let one =document.getElementById("one");
+one.addEventListener("click", function(){
+  // console.log("hi");
+  let body = document.querySelector('body');
+  body.classList.remove('active1');
+  body.classList.remove('active2');
+ })
+
+ let three = document.getElementById("three");
+ three.addEventListener("click", function(){
+  //  console.log("hi");
+  let body = document.querySelector('body');
+  body.classList.add('active2');
+  body.classList.remove('active1');
+
+ })
